610f86f87ac3e442f4186b0e6acf3fab925fa429
alliedbrother <saiakhil121@gmail.com>
2023-11-25 02:20:06 -0500

    base

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import numpy as np
 import os 
 from ydata_profiling import ProfileReport
 from PIL import Image
-import pandas_profiling
-import ydata_profiling
+
 from streamlit_pandas_profiling import st_profile_report
 import matplotlib
 import pycaret.classification as pyc 
